b75a8cbf7543f7a917c9081650a5864feac4cf6b
John Michael Paraiso <johnmparaiso@gmail.com>
2020-06-03 19:37:01 +0100

    Update EditorSnap.cs

--- Assets/EditorSnap.cs
+++ Assets/EditorSnap.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿// The PrintAwake script is placed on a GameObject.  The Awake function is
+// called when the GameObject is started at runtime.  The script is also
+// called by the Editor.  An example is when the Scene is changed to a
+// different Scene in the Project window.
+// The Update() function is called, for example, when the GameObject transform
+// position is changed in the Editor.
+
 using UnityEngine;
 
-public class EditorSnap : MonoBehaviour
+[ExecuteInEditMode]
+public class PrintAwake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        Debug.Log("Editor causes this Awake");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Editor causes this Update");
     }
 }
