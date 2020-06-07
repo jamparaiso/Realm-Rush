5c8a0f238e2e478a0dd1d39c7e92a1b77e444e65
John Michael Paraiso <johnmparaiso@gmail.com>
2020-06-08 00:00:32 +0100

    added enemy movement
    in prep for Breadth First Search Algorithm

--- Assets/MoveEnemy.cs
+++ Assets/MoveEnemy.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 public class MoveEnemy : MonoBehaviour
 {
 
+    [SerializeField] List<WayPoint> path;
+
+    float dwellTime = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FollowPath());
     }
+
+
+    IEnumerator FollowPath()
+    {
+        print("Starting Patrol...");
+
+        foreach (WayPoint wayPoint in path)
+        {
+            transform.position = wayPoint.transform.position;
+            print("Visiting Block: " + wayPoint.name);
+            yield return new WaitForSeconds(dwellTime);
+        }
+
+        print("Ending Patrol..");
+    }
+
 
     // Update is called once per frame
     void Update()
